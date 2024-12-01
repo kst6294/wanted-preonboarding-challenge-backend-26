d3fc19c9c253496b8d3bec13f67b08840e503786
Sermadl <rlatpdms1014@gmail.com>
2024-12-01 15:17:34 +0900

    feature: CreatedAt, UpdatedAt 기록을 위한 빈 등록

--- wantedPayment/src/main/java/org/wantedpayment/WantedPaymentApplication.java
+++ wantedPayment/src/main/java/org/wantedpayment/WantedPaymentApplication.java
@@ -2,8 +2,10 @@ package org.wantedpayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WantedPaymentApplication {
 
     public static void main(String[] args) {
