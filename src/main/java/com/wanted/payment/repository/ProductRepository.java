30f51dc8511746c80a804e4461fb0aa79e5ef57f
jongeuni <lje0818ovo@naver.com>
2024-12-02 00:30:06 +0900

    [ADD] 리드미 수정

--- src/main/java/com/wanted/payment/repository/ProductRepository.java
+++ src/main/java/com/wanted/payment/repository/ProductRepository.java
@@ -4,7 +4,6 @@ import com.wanted.payment.schema.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findByIdIn(List<Integer> ids);
